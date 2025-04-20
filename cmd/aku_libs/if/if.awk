c6839a0248335e4aae11b5626c9c31d4604bfd82
puutaro <hoperstep@gmail.com>
2025-04-20 17:09:47 +0900

    add field and row feature to if

---
+++ cmd/aku_libs/if/if.awk
@@ -0,0 +1,38 @@
+
+function exec_if(\
+	target_str_arg,\
+	regex_con,\
+	cmd_con,\
+	el_list_arg,\
+	max_nf_num\
+){
+	target_str_inner = target_str_arg
+	if(target_str_inner !~  regex_con){
+		return target_str_inner 
+	}
+	gsub(/x22/, "\\\x22", target_str_inner)
+	echo_cmd = sprintf("echo \x22%s\x22", target_str_inner)
+	cmd = sprintf(\
+		"%s | %s", \
+		echo_cmd, \
+		cmd_con\
+	)
+	for(i=0; i<=max_nf_num;i++){
+		# print "max_nf_num "max_nf_num
+		# print "i "i
+		# print "## el_list_arg "el_list_arg[i]
+		gsub(sprintf("@\\{%d\\}",i), el_list_arg[i], cmd)
+	}
+	last_output=""
+	line_num = 1
+	while ((cmd | getline output_line) > 0) {
+		if(line_num == 1){
+			last_output = output_line
+		} else {
+			last_output = sprintf("%s\n%s", last_output, output_line)
+		}
+		line_num++
+	}
+	close(cmd)
+	return last_output
+} 
--- cmd/aku_libs/if/if.sh
+++ cmd/aku_libs/if/if.sh
@@ -10,20 +10,34 @@ read_args_for_if(){
 		--help|-h)
 			HELP="${1}"
 			;;
-		--delimitter|-d)
+		--field-num|-f)
+			FIELD_NUM_LIST_CON="${FIELD_NUM_LIST_CON}${NUM_LIST_CON_SEPARATOR}${2}"
+			shift
+			;;
+		---delimitter|-d)
 			DELIMITTER="${2}"
 			shift
+			;;
+		-row-num|-r)
+			ROW_NUM_LIST_CON="${ROW_NUM_LIST_CON}${NUM_LIST_CON_SEPARATOR}${2}"
+			shift
+			;;
+		--on-negative|-n)
+			ON_NEGATIVE="on"
 			;;
 		-*)
 			echo "no option: ${1}"
 			exit 1
 			;;
 		*)	
-			if [ ${count_arg_input} -eq 1  ];then
-				REGEX_CON="${1:-}"
-			elif [ ${count_arg_input} -ge 2 ]; then
-				CMD_CON="${CMD_CON}${ARG_SEPARATOR}${1:-}"
-			fi
+			case ${count_arg_input} in
+				1)
+					REGEX_CON="${1:-}"
+					;;
+				2)
+					CMD_CON="${1:-}"
+					;;
+			esac
 			count_arg_input=$((count_arg_input + 1))
 			;;
 	esac
@@ -86,6 +100,98 @@ display_help_for_if(){
 				print ""
 				print "echo \x22aa\nbb\x22 | aku if  \x22aa\x22 \x22touch @{0}; echo @{0}\x22"
 				print ""
+				print "#### --field-num|-f"
+				print ""
+				print "target field"
+				print ""
+				print "- Ex single field (default: all)"
+				print ""
+				print "```sh.sh"
+				print "echo \x22aa    bb   cc    #dd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 -f \x222\x22"
+				print "```"
+				print ""
+				print "- Ex multiple field"
+				print ""
+				print "```sh.sh"
+				print "echo \x22aa    bb   cc    #dd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 -f \x221\x22 -f \x223-4\x22"
+				print "```"
+				print ""
+				print "- Ex multiple field by end range"
+				print ""
+				print "```sh.sh"
+				print "echo \x22aa    bb   cc    #dd\x22 | aku if -f \x22.*\x22 \x22echo @[1}\x22 \x221\x22 -f \x22-4\x22"
+				print "```"
+				print ""
+				print "- Ex multiple field by end range"
+				print ""
+				print "```sh.sh"
+				print "echo \x22aa    bb   cc    #dd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 -f \x221\x22 -f \x222-\x22"
+				print "```"
+				print ""
+				print "#### --row-num|-r"
+				print ""
+				print "target row (default: all)"
+				print ""
+				print "- Ex single row"
+				print ""
+				print "```sh.sh"
+				print "echo ~\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 -r \x222\x22"
+				print "```"
+				print ""
+				print "- Ex multiple row"
+				print ""
+				print "```sh.sh"
+				print "echo \x22~\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 -r \x221\x22 -r \x223-4\x22"
+				print "```"
+				print ""
+				print "- Ex multiple row by end range"
+				print ""
+				print "```sh.sh"
+				print "echo \x22~\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 -r \x221\x22 -r \x22-4\x22"
+				print "```"
+				print ""
+				print "- Ex range specify -end"
+				print ""
+				print "```.sh.sh"
+				print "echo \x22aaA\nbBb\nccC\nDdd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 \x22[a-z]\x22 -r -2"
+				print "->"
+				print "A\nB\nccC\nDdd"
+				print "```"
+				print ""
+				print "- Ex range specify -start "
+				print ""
+				print "```.sh.sh"
+				print "echo \x22aaA\nbwBb\nccC\nDdd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 \x22[a-z]x22 -r 2-"
+				print "->"
+				print "aaA\nB\nC\nD"
+				print "```"
+				print ""
+				print "- Ex range specify start-end "
+				print ""
+				print "```.sh.sh"
+				print "echo \x22aaA\nbBb\nccC\nDdd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 \x22[a-z]\x22 -r 2-4"
+				print "->"
+				print "aaA\nB\nC\nD"
+				print "```"
+				print ""
+				print "- Ex multiple "
+				print ""
+				print "```.sh.sh"
+				print "echo \x22aaA\nbBb\nccC\nDdd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 \x22[a-z]\x22 -r 1 -r 3-4"
+				print "->"
+				print "A\nbBb\ncC\nD"
+				print "```"
+				print ""
+				print "#### --on-negative|-n"
+				print ""
+				print "negative to field num and row num"
+				print ""
+				print "- Ex"
+				print ""
+				print "```sh.sh"
+				print "echo \x22aaAAAbbAAAccAAA#dd\x22 | aku if \x22.*\x22 \x22echo @[1}\x22 -n -f \x222\x22 -d \x2AAA\x22"
+				print "```"
+				print ""
 			}' | less
 			exit 0
 			;;
@@ -93,43 +199,153 @@ display_help_for_if(){
 }
 
 exec_if(){
+	local if_awk_path="${IF_DIR_PATH}/if.awk"
+	local contain_num_separator=","
 	local max_nf_num=$(\
 		echo "${CONTENTS}" \
 		| awk  -F "${DELIMITTER}" '{print NF; exit}'\
 	)
 	echo "${CONTENTS}"\
 	| ${AWK_PATH} \
+		-i "${AWK_LIST_FUNCS_PATH}"\
+		-i "${if_awk_path}"\
 		-F "${DELIMITTER}" \
+		-v src_con="${CONTENTS}"\
+		-v DELIMITTER="${DELIMITTER}"\
+		-v CONTAIN_NUM_SEPARATOR="${contain_num_separator}"\
+	 	-v ROW_NUM_LIST_CON="${ROW_NUM_LIST_CON#${NUM_LIST_CON_SEPARATOR}}" \
+	 	-v FIELD_NUM_LIST_CON="${FIELD_NUM_LIST_CON#${NUM_LIST_CON_SEPARATOR}}" \
+	 	-v NUM_LIST_CON_SEPARATOR="${NUM_LIST_CON_SEPARATOR}" \
 		-v max_nf_num="${max_nf_num}"\
 		-v REGEX_CON="${REGEX_CON}"\
 		-v CMD_CON="${CMD_CON}"\
-	'{
+	 	-v ON_NEGATIVE="${ON_NEGATIVE}"\
+	'BEGIN{
+			DISPLAY_FIELD_NUM_CON = make_list_from_muti_list_con(\
+				FIELD_NUM_LIST_CON, \
+				max_nf_num,\
+				NUM_LIST_CON_SEPARATOR,\
+				CONTAIN_NUM_SEPARATOR\
+			)
+			gsub(CONTAIN_NUM_SEPARATOR, "", FIELD_NUM_LIST_CON)
+			DISPLAY_FIELD_NUM_CON = trim_separator(DISPLAY_FIELD_NUM_CON, CONTAIN_NUM_SEPARATOR)
+			DISPLAY_FIELD_NUM_CON = sort_list_con(DISPLAY_FIELD_NUM_CON, CONTAIN_NUM_SEPARATOR)
+			DISPLAY_FIELD_NUM_CON = remove_dup_el(DISPLAY_FIELD_NUM_CON, CONTAIN_NUM_SEPARATOR)
+			# print "DISPLAY_FIELD_NUM_CON "DISPLAY_FIELD_NUM_CON
+
+			max_lines = split(src_con, _line_array, "\n")
+			# print "CONTAIN_NUM_SEPARATOR "CONTAIN_NUM_SEPARATOR
+			# print "ROW_NUM_LIST_CON "ROW_NUM_LIST_CON
+			# print "NUM_LIST_CON_SEPARATOR "NUM_LIST_CON_SEPARATOR
+			DISPLAY_ROW_NUM_CON = make_list_from_muti_list_con(\
+				ROW_NUM_LIST_CON, \
+				max_lines,\
+				NUM_LIST_CON_SEPARATOR,\
+				CONTAIN_NUM_SEPARATOR\
+			)
+			DISPLAY_ROW_NUM_CON = trim_separator(DISPLAY_ROW_NUM_CON, CONTAIN_NUM_SEPARATOR)
+			DISPLAY_ROW_NUM_CON = sort_list_con(DISPLAY_ROW_NUM_CON, CONTAIN_NUM_SEPARATOR)
+			DISPLAY_ROW_NUM_CON = remove_dup_el(DISPLAY_ROW_NUM_CON, CONTAIN_NUM_SEPARATOR)
+			# print "11 DISPLAY_ROW_NUM_CON " DISPLAY_ROW_NUM_CON
+			gsub(CONTAIN_NUM_SEPARATOR, "", ROW_NUM_LIST_CON)
+			# print "ON_NEGATIVE "ON_NEGATIVE
+	}{
 		# print "$0 "$0
-		if($0 !~ REGEX_CON){
+		match_row_num = sprintf("%s%s%s", CONTAIN_NUM_SEPARATOR, NR, CONTAIN_NUM_SEPARATOR)
+		is_pass_row_num = 0
+		if(\
+			index(\
+				sprintf(\
+					"%s%s%s",\
+					CONTAIN_NUM_SEPARATOR,\
+					DISPLAY_ROW_NUM_CON,\
+					CONTAIN_NUM_SEPARATOR\
+				),\
+				match_row_num\
+			) == 0\
+			&& DISPLAY_ROW_NUM_CON\
+		){
+			is_pass_row_num = 1
+		}
+		if(\
+			!ON_NEGATIVE \
+			&& is_pass_row_num\
+		){
+			print $0
+			next
+		}else if (\
+			ON_NEGATIVE \
+			&& ROW_NUM_LIST_CON\
+			&& !is_pass_row_num\
+		) {
 			print $0
 			next
 		}
-		gsub(/x22/, "\\\x22", $0)
-		echo_cmd = sprintf("echo \x22%s\x22", $0)
-		cmd = sprintf(\
-			"%s | %s", \
-			echo_cmd, \
-			CMD_CON\
-		)
+		target_str = ""
+		if(!FIELD_NUM_LIST_CON){
+			target_str = $0
+		}
+
+		el_list[0] = $0
 		for(l=0; l<=max_nf_num;l++){
-			gsub(sprintf("@\\{%d\\}",l), $l, cmd)
+			el_list[l] = $l
 		}
-		last_output=""
-		line_num = 1
-		while ((cmd | getline line) > 0) {
-			if(line_num == 1){
-				last_output = line
-			} else {
-				last_output = sprintf("%s\n%s", last_output, line)
+
+		if(FIELD_NUM_LIST_CON == ""){
+			print exec_if(\
+				target_str,\
+				REGEX_CON,\
+				CMD_CON,\
+				el_list,\
+				max_nf_num\
+			)
+			next
+		}
+		line = ""
+		for(l=1;l<=NF;l++){
+			is_pass_field_num = 0
+			match_field_num = sprintf("%s%s%s", CONTAIN_NUM_SEPARATOR, l, CONTAIN_NUM_SEPARATOR)
+			if(\
+				index(\
+					sprintf(\
+						"%s%s%s", \
+						CONTAIN_NUM_SEPARATOR,\
+						DISPLAY_FIELD_NUM_CON,\
+						CONTAIN_NUM_SEPARATOR\
+					),
+					match_field_num)  == 0\
+				&& DISPLAY_FIELD_NUM_CON\
+			) {
+				is_pass_field_num = 1
 			}
-			line_num++
+			if(\
+				!ON_NEGATIVE \
+				&& is_pass_field_num\
+			){
+				line = sprintf("%s%s%s",line, $l, DELIMITTER)
+				continue
+			}else if (\
+				ON_NEGATIVE \
+				&& FIELD_NUM_LIST_CON\
+				&& !is_pass_field_num\
+			) {
+				line = sprintf("%s%s%s",line, $l, DELIMITTER)
+				continue
+			}
+			el = exec_if(\
+				$l,\
+				REGEX_CON,\
+				CMD_CON,\
+				el_list,\
+				max_nf_num\
+			)
+			if(l == NF){
+				line = sprintf("%s%s", line, el)
+				continue
+			}
+			line = sprintf("%s%s%s",line, el, DELIMITTER)
+			# print "## line"line"CC"
 		}
-		print last_output
-		close(cmd)
+		print line
 	}'
 }
